9f404e4a6c20ac5774866c543fb8121d185efcf9
sayakakawa <brightbright590@gmail.com>
2025-12-08 14:10:17 +0900

    kagitani-system モード追加

--- kagitani-system/php/connect_db.php
+++ kagitani-system/php/connect_db.php
@@ -2,16 +2,25 @@
 
 	
 $db_host = "localhost";  // DBサーバのurl
+$db_port = 8889;  // ポート番号
 $db_user = "root";
 $db_password = "root";
 $db_dbname = "forest_platform";
 
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname);
-	if ($mysqli->connect_error) {
-	  print('<p>データベースへの接続に失敗しました。</p>' . $mysqli->connect_error);
-	  exit();
-	} else {
-    	$mysqli->set_charset("utf8");
-	}
+//アプリケーションサーバーにアップロードするとき．
+// $db_host = "localhost";  // DBサーバのurl
+// $db_port = 3306;
+// $db_user = "root";
+// $db_password = "kslabkslab";
+// $db_dbname = "forest_platform";
+
+// mysqlへの接続
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname, $db_port);
+if ($mysqli->connect_error) {
+	print('<p>データベースへの接続に失敗しました。</p>' . $mysqli->connect_error);
+	exit();
+} else {
+	$mysqli->set_charset("utf8");
+}
 
 ?>
