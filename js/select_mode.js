86be1a47e47d0ee6f988bdc662d589c40110121a
sayakakawa <brightbright590@gmail.com>
2025-12-09 13:25:13 +0900

    島岡排除

--- js/select_mode.js
+++ js/select_mode.js
@@ -20,7 +20,8 @@ document.getElementById("selectmodebutton").addEventListener('click', function()
             window.location.href = "fukushima-system/select_sheet.php";
           }else if(selectmodeoption[i].value === "6"){
             // ここ変える必要あり
-            window.location.href = "shimaoka-system/select_sheet.php";
+            console.log("島岡システム：未搭載")
+            // window.location.href = "shimaoka-system/select_sheet.php";
           }
           return;
         }
