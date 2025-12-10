21e2041d63f8023922e1602711e1a2ff8cb97872
WakanaFukushima <wakana2940@gmail.com>
2025-12-11 01:31:46 +0900

    DB調整

--- forest-mrn/php/connect_db.php
+++ forest-mrn/php/connect_db.php
@@ -1,12 +1,12 @@
 <?php
 
-
+	
 	//ローカルPCから接続するとき．
-	$db_host = "localhost";  // DBサーバのurl
-	$db_port = 8889;  // ポート番号
-	$db_user = "root";
-	$db_password = "root";
-	$db_dbname = "forest_platform";
+	// $db_host = "localhost";  // DBサーバのurl
+	// $db_port = 8889;  // ポート番号
+	// $db_user = "root";
+	// $db_password = "root";
+	// $db_dbname = "forest_platform";
 
 	// デスクトップPCに接続
 	// $db_host = "10.250.63.6";  // ホスト名のみ
@@ -16,16 +16,14 @@
 	// $db_dbname = "forest_platform";
 
 	//アプリケーションサーバーにアップロードするとき．
-	// $db_host = "localhost";  // DBサーバのurl
-	// $db_port = 3306;
-	// $db_user = "root";
-	// $db_password = "kslabkslab";
-	// $db_dbname = "forest_platform";
+	$db_host = "localhost";  // DBサーバのurl
+	$db_port = 3306;
+	$db_user = "root";
+	$db_password = "kslabkslab";
+	$db_dbname = "forest_platform";
 
-
-	// // mysqlへの接続
+	// mysqlへの接続
 	$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname, $db_port);
-	// $mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname);
 	if ($mysqli->connect_error) {
 	  print('<p>データベースへの接続に失敗しました。</p>' . $mysqli->connect_error);
 	  exit();
