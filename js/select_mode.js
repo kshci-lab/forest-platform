8712af9eb3cf8d1984c6de483978a587d767d36b
WakanaFukushima <wakana2940@gmail.com>
2026-01-19 23:24:34 +0900

    シート経由なくす？

--- js/select_mode.js
+++ js/select_mode.js
@@ -16,8 +16,8 @@ document.getElementById("selectmodebutton").addEventListener('click', function()
             // ここ変える必要あり
             window.location.href = "./kagitani-system/select_sheet.php";
           }else if(selectmodeoption[i].value === "5"){
-            // ここ変える必要あり
-            window.location.href = "./fukushima-system/select_sheet.php";
+            // 福島モードは共有知モードを直接起動
+            window.location.href = "./fukushima-system/index.php?mode=shared";
           }else if(selectmodeoption[i].value === "6"){
             // ここ変える必要あり
             console.log("島岡システム：未搭載")
