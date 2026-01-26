f2a31221e08b1d0ba142043c325cb4fdc502c374
sayakakawa <146063396+sayakakawa@users.noreply.github.com>
2026-01-26 23:42:31 +0900

    Revert "Reapply "Reapply "DB"""
    This reverts commit d9aa0c869c6d1b6c6abbd2610caf5e648fffb098.

--- forest-mrn/php/connect_db.php
+++ forest-mrn/php/connect_db.php
@@ -2,11 +2,11 @@
 
 	
 	//ローカルPCから接続するとき．
-	// $db_host = "localhost";  // DBサーバのurl
-	// $db_port = 8889;  // ポート番号
-	// $db_user = "root";
-	// $db_password = "root";
-	// $db_dbname = "forest_platform";
+	$db_host = "localhost";  // DBサーバのurl
+	$db_port = 8889;  // ポート番号
+	$db_user = "root";
+	$db_password = "root";
+	$db_dbname = "forest_platform";
 
 	// デスクトップPCに接続
 	// $db_host = "10.250.63.6";  // ホスト名のみ
@@ -16,11 +16,11 @@
 	// $db_dbname = "forest_platform";
 
 	//アプリケーションサーバーにアップロードするとき．
-	$db_host = "localhost";  // DBサーバのurl
-	$db_port = 3306;
-	$db_user = "root";
-	$db_password = "kslabkslab";
-	$db_dbname = "forest_platform";
+	// $db_host = "localhost";  // DBサーバのurl
+	// $db_port = 3306;
+	// $db_user = "root";
+	// $db_password = "kslabkslab";
+	// $db_dbname = "forest_platform";
 
 	// mysqlへの接続
 	$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname, $db_port);
