1fe98ea3f0a9eb610f49987a6fcc05c699fd0833
WakanaFukushima <wakana2940@gmail.com>
2026-02-13 09:12:26 +0900

    connect_db.php作った

---
+++ fukushima-system/php/connect_db.php
@@ -0,0 +1,34 @@
+<?php
+
+	
+	//ローカルPCから接続するとき．
+	// $db_host = "localhost";  // DBサーバのurl
+	// $db_port = 8889;  // ポート番号
+	// $db_user = "root";
+	// $db_password = "root";
+	// $db_dbname = "forest_platform";
+
+	// デスクトップPCに接続
+	// $db_host = "10.250.63.6";  // ホスト名のみ
+	// $db_port = 8889;  // ポート番号
+	// $db_user = "root";
+	// $db_password = "your_password";
+	// $db_dbname = "forest_platform";
+
+	//アプリケーションサーバーにアップロードするとき．
+	$db_host = "localhost";  // DBサーバのurl
+	$db_port = 3306;
+	$db_user = "root";
+	$db_password = "kslabkslab";
+	$db_dbname = "forest_platform";
+
+	// mysqlへの接続
+	$mysqli = new mysqli($db_host, $db_user, $db_password, $db_dbname, $db_port);
+	if ($mysqli->connect_error) {
+	  print('<p>データベースへの接続に失敗しました。</p>' . $mysqli->connect_error);
+	  exit();
+	} else {
+    	$mysqli->set_charset("utf8");
+	}
+
+?>
