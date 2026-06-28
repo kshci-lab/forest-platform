8eea97557f7e45f310e96c8097f87e458a046af8
Rena Kagitani <sp25519r@st.omu.ac.jp>
2026-06-28 13:24:30 +0900

    Update index.php

--- kagitani-system/index.php
+++ kagitani-system/index.php
@@ -735,7 +735,7 @@ try {
                                     <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
                                 </svg>
                             </button>
-                            <div class="dropdown-menu">
+                            <!-- <div class="dropdown-menu"> -->
                                 <!-- <div class="dropdown-item language-toggle-item" style="padding: 12px 20px; cursor:default; box-sizing:border-box;">
                                      <div style="display:flex; align-items:center; background:rgba(0,0,0,0.05); border-radius:8px; padding:4px; width:100%; box-sizing:border-box; position:relative;"> -->
                                         <!-- Animated background for the active state -->
@@ -811,7 +811,7 @@ try {
                                         ログアウト
                                     </button>
                                 </form>
-                            </div>
+                            <!-- </div> -->
                         </div>
                 </div>
             </div>
