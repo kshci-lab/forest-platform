e79515b694deb5a1d185ecec0e2925c7c334b75b
Rena Kagitani <sp25519r@st.omu.ac.jp>
2026-07-13 10:55:56 +0900

    連結基盤実装

---
+++ php/sso_local.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'client_id' => '019f592e-bcf5-73f1-b618-151434c71c4f',
+    'client_secret' => 'kzOmOGPicmji77hZ5ikBHXNQUBOPVgnBEZywUbtG',
+    'base_url' => 'http://localhost:8888/forest-platform',
+    'redirect_uri' => 'http://localhost:8888/forest-platform/auth/callback',
+);
